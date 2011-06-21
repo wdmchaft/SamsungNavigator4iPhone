37b7c699b181811e705f1665d69eaed66d4716f4
Apple Mini <wenxiao.zhang@opentide.com.cn>
2011-06-21 17:15:52 +0800

    2011-06-21 Static Layout Done

--- Classes/SamsungNavigatorViewController.h
+++ Classes/SamsungNavigatorViewController.h
@@ -10,17 +10,27 @@
 
 @interface SamsungNavigatorViewController : UIViewController
 {
-	IBOutlet UITextView * weibo1;
-
+	
 }
-
-@property(nonatomic, retain) IBOutlet UITextField *weibo1;
 
 - (IBAction) goToURL1: (id) sender;
 
 - (IBAction) goToURL2: (id) sender;
 
 - (IBAction) goToWeibo1: (id) sender;
+
+- (IBAction) goToWeibo2: (id) sender;
+
+- (IBAction) goToWeibo3: (id) sender;
+
+- (IBAction) goToWeibo4: (id) sender;
+
+- (IBAction) goToRenren1: (id) sender;
+
+- (IBAction) goToRenren2: (id) sender;
+
+- (IBAction) goToRenren3: (id) sender;
+
 
 @end
 
--- Classes/SamsungNavigatorViewController.m
+++ Classes/SamsungNavigatorViewController.m
@@ -13,23 +13,50 @@
 
 - (IBAction) goToURL1: (id) sender
 {		
-	NSLog(@"ZZZZZ");
-
 	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://m.samsung.com/cn"]];
 }
 
 - (IBAction) goToURL2: (id) sender
 {	
-	NSLog(@"YYYYYY");
-
 	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://shop.samsung.com.cn"]];
 }
 
 - (IBAction) goToWeibo1: (id) sender;
 {
-	NSLog(@"wwwwwww");
 	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://weibo.cn/dpool/ttt/home.php?uid=1660521332d=samsungelectronic"]];
 }
+
+- (IBAction) goToWeibo2: (id) sender;
+{
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://weibo.cn/dpool/ttt/home.php?uid=1760637645d=samsunge3d"]];
+}
+
+- (IBAction) goToWeibo3: (id) sender;
+{
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://weibo.cn/dpool/ttt/home.php?uid=2031482343d=2031482343"]];
+}
+
+- (IBAction) goToWeibo4: (id) sender;
+{
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://weibo.cn/dpool/ttt/home.php?uid=1709253524d=samsungdi"]];
+}
+
+- (IBAction) goToRenren1: (id) sender;
+{
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://page.renren.com/600395406"]];
+}
+
+- (IBAction) goToRenren2: (id) sender;
+{
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://page.renren.com/600441106"]];
+}
+
+
+- (IBAction) goToRenren3: (id) sender;
+{
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://page.renren.com/samsungdi"]];
+}
+
 
 /*
 // The designated initializer. Override to perform setup that is required before the view is loaded.
@@ -53,6 +80,7 @@
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
+	//UITextView textView = [[UITextView alloc] initWithCoder: 
 //	weibo1.editable = NO;
 }
 
